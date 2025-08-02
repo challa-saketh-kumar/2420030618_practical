96cdf50a041a331bc1ca3743fdf5d8070d4d2af0
challa-saketh-kumar <2420030618@klh.edu.in>
2025-08-02 09:47:04 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,11 @@
+package MyProject;
+
+public class second {
+	public static void main(String[] args) {
+		String name = "Challa Saketh Kumar";
+		String dep = "CSE";
+		int roll_no = "2420030618";
+		int phone_no = "8885406410";
+		String address = "h.no: 3-410, plot.no:1293, suraram colony";
+	}
+}
